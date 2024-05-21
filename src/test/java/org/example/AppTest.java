bf66441434e387faf24280922f093444c7fcee50
Leonid Prokopev <leo.messi32@yandex.ru>
2024-05-21 17:01:58 +0300

    done App and tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,203 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+    public String expectations_path = "src/test/resources/expectations.txt";
+
+    public static List<BigInteger[]> readExpectations(String fileName) {
+        List<BigInteger[]> expectations = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                BigInteger[] nums = new BigInteger[parts.length];
+                for (int i = 0; i < parts.length; i++) {
+                    nums[i] = new BigInteger(parts[i]);
+                }
+                expectations.add(nums);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+        return expectations;
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    private void runTest(int index, String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String[] nums = reader.readLine().split(" ");
+            Assert.assertEquals(expectations.get(index)[0], App._min(nums));
+            Assert.assertEquals(expectations.get(index)[1], App._max(nums));
+            Assert.assertEquals(expectations.get(index)[2], App._sum(nums));
+            Assert.assertEquals(expectations.get(index)[3], App._mult(nums));
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    List<BigInteger[]> expectations = readExpectations(expectations_path);
+
+    @Test
+    public void test1() {
+        runTest(0, "src/test/resources/test_1.txt");
+    }
+
+    @Test
+    public void test2() {
+        runTest(1, "src/test/resources/test_2.txt");
+    }
+
+    @Test
+    public void test3() {
+        runTest(2, "src/test/resources/test_3.txt");
+    }
+
+    @Test
+    public void test4() {
+        runTest(3, "src/test/resources/test_4.txt");
+    }
+
+    @Test
+    public void test5() {
+        runTest(4, "src/test/resources/test_5.txt");
+    }
+
+    @Test
+    public void test6() {
+        runTest(5, "src/test/resources/test_6.txt");
+    }
+
+    @Test
+    public void test7() {
+        runTest(6, "src/test/resources/test_7.txt");
+    }
+
+    @Test
+    public void test8() {
+        runTest(7, "src/test/resources/test_8.txt");
+    }
+
+    @Test
+    public void test9() {
+        runTest(8, "src/test/resources/test_9.txt");
+    }
+
+    @Test
+    public void test10() {
+        runTest(9, "src/test/resources/test_10.txt");
+    }
+
+    @Test
+    public void test11() {
+        runTest(10, "src/test/resources/test_11.txt");
+    }
+
+    @Test
+    public void test12() {
+        runTest(11, "src/test/resources/test_12.txt");
+    }
+
+    @Test
+    public void test13() {
+        runTest(12, "src/test/resources/test_13.txt");
+    }
+
+    @Test
+    public void test14() {
+        runTest(13, "src/test/resources/test_14.txt");
+    }
+
+    @Test
+    public void test15() {
+        runTest(14, "src/test/resources/test_15.txt");
+    }
+
+    @Test
+    public void test16() {
+        runTest(15, "src/test/resources/test_16.txt");
+    }
+
+    @Test
+    public void test17() {
+        runTest(16, "src/test/resources/test_17.txt");
+    }
+
+    @Test
+    public void test18() {
+        runTest(17, "src/test/resources/test_18.txt");
+    }
+
+    @Test
+    public void test19() {
+        runTest(18, "src/test/resources/test_19.txt");
+    }
+
+    @Test
+    public void test20() {
+        runTest(19, "src/test/resources/test_20.txt");
+    }
+
+    @Test
+    public void test21() {
+        runTest(20, "src/test/resources/test_21.txt");
+    }
+
+    @Test
+    public void test22() {
+        runTest(21, "src/test/resources/test_22.txt");
+    }
+
+    @Test
+    public void test23() {
+        runTest(22, "src/test/resources/test_23.txt");
+    }
+
+    @Test
+    public void test24() {
+        runTest(23, "src/test/resources/test_24.txt");
+    }
+
+    @Test
+    public void test25() {
+        runTest(24, "src/test/resources/test_25.txt");
+    }
+
+    @Test
+    public void test26() {
+        runTest(25, "src/test/resources/test_26.txt");
+    }
+
+    @Test
+    public void test27() {
+        runTest(26, "src/test/resources/test_27.txt");
+    }
+
+    @Test
+    public void test28() {
+        runTest(27, "src/test/resources/test_28.txt");
+    }
+
+    @Test
+    public void test29() {
+        runTest(28, "src/test/resources/test_29.txt");
+    }
+
+    @Test
+    public void test30() {
+        runTest(29, "src/test/resources/test_30.txt");
+    }
+	@Test
+    public void test31() {
+        runTest(30, "src/test/resources/test_31.txt");
     }
 }
