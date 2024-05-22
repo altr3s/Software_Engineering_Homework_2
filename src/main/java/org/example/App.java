ab494bc00e1c6d599ebe3240a5e6d2efa938ba1e
Leonid Prokopev <leo.messi32@yandex.ru>
2024-05-22 23:59:43 +0300

    added ERROR with empty input file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,22 +8,29 @@ import java.math.BigInteger;
 public class App {
 
 	public static void main(String[] args) {
-		String fileName = "src/main/resources/input.txt";
+        String fileName = "src/main/resources/input.txt";
 
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(fileName));
-			String[] nums = reader.readLine().split(" ");
-			System.out.println("Минимальное число: " + _min(nums));
-			System.out.println("Максимальное число: " + _max(nums));
-			System.out.println("Сумма чисел: " + _sum(nums));
-			System.out.println("Произведение чисел: " + _mult(nums));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line = reader.readLine();
 
-		} catch (IOException e) {
-			System.out.println("Ошибка при чтении файла: " + e.getMessage());
+            if (line == null || line.trim().isEmpty()) {
+                System.out.println("Файл пуст или содержит только пробельные символы. "
+					+ "Пожалуйста, проверьте файл 'input.txt'");
+                return;
+            }
 
+            String[] nums = line.split(" ");
+            System.out.println("Минимальное число: " + _min(nums));
+            System.out.println("Максимальное число: " + _max(nums));
+            System.out.println("Сумма чисел: " + _sum(nums));
+            System.out.println("Произведение чисел: " + _mult(nums));
 
-		}
-	}
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
 
 	public static BigInteger _min(String[] numbers) {
 		try {
