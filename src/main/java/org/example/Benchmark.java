65fca383d3584562ca5c0f253da2b967ee464e03
Leonid Prokopev <leo.messi32@yandex.ru>
2024-05-24 00:29:33 +0300

    Update README.md; edited empty file test; and microfix

--- src/main/java/org/example/Benchmark.java
+++ src/main/java/org/example/Benchmark.java
@@ -8,64 +8,64 @@ import java.io.IOException;
 
 public class Benchmark {
 
-    public static void main(String[] args) {
-        String[] array = {"min", "max", "sum", "mult"};
-        for (int i = 1; i < 26; i++) {
-            String filePath = "src/main/resources/benchmark_tests/test-" + i + ".txt";
+	public static void main(String[] args) {
+		String[] array = {"min", "max", "sum", "mult"};
+		for (int i = 1; i < 26; i++) {
+			String filePath = "src/main/resources/benchmark_tests/test-" + i + ".txt";
 
-            for (int j = 0; j < array.length; j++) {
-                String testType = array[j];
-                String output = "src/main/resources/benchmark_tests/results-" + testType + ".txt";
-                long result = runTest(filePath, testType);
-                if (result != -1) {
-                    try (FileWriter writer = new FileWriter(output, true)) {
-                        writer.write(i + ") " + result + " ns\n");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
+			for (int j = 0; j < array.length; j++) {
+				String testType = array[j];
+				String output = "src/main/resources/benchmark_tests/results-" + testType + ".txt";
+				long result = runTest(filePath, testType);
+				if (result != -1) {
+					try (FileWriter writer = new FileWriter(output, true)) {
+						writer.write(i + ") " + result + " ns\n");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
 
-    public static long runTest(String filePath, String testType) {
-        long startTime = System.nanoTime();
+	public static long runTest(String filePath, String testType) {
+		long startTime = System.nanoTime();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line = reader.readLine();
-            if (line == null || line.trim().isEmpty()) {
-                System.out.println("Файл пуст или содержит только пробельные символы: " + filePath);
-                return -1;
-            }
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			String line = reader.readLine();
+			if (line == null || line.trim().isEmpty()) {
+				System.out.println("Файл пуст или содержит только пробельные символы: " + filePath);
+				return -1;
+			}
 
-            String[] nums = line.split(" ");
+			String[] nums = line.split(" ");
 
-            switch (testType) {
-                case "min":
-                    App._min(nums);
-                    break;
-                case "max":
-                    App._max(nums);
-                    break;
-                case "sum":
-                    App._sum(nums);
-                    break;
-                case "mult":
-                    App._mult(nums);
-                    break;
-                default:
-                    throw new IllegalArgumentException("Неверный тип теста: " + testType);
-            }
+			switch (testType) {
+				case "min":
+					App._min(nums);
+					break;
+				case "max":
+					App._max(nums);
+					break;
+				case "sum":
+					App._sum(nums);
+					break;
+				case "mult":
+					App._mult(nums);
+					break;
+				default:
+					throw new IllegalArgumentException("Неверный тип теста: " + testType);
+			}
 
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла: " + e.getMessage());
-            return -1;
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-            return -1;
-        }
+		} catch (IOException e) {
+			System.out.println("Ошибка при чтении файла: " + e.getMessage());
+			return -1;
+		} catch (IllegalArgumentException e) {
+			System.out.println("Ошибка: " + e.getMessage());
+			return -1;
+		}
 
-        long endTime = System.nanoTime();
-        return endTime - startTime;
-    }
+		long endTime = System.nanoTime();
+		return endTime - startTime;
+	}
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,76 +1,84 @@
 package org.example;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.*;
 
 public class AppTest {
+
 	public static List<BigInteger> readExpectations(String fileName) {
-        List<BigInteger> expectations = new ArrayList<>();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String[] line = reader.readLine().split(" ");
-            for (String num: line) {
+		List<BigInteger> expectations = new ArrayList<>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			String[] line = reader.readLine().split(" ");
+			for (String num : line) {
 				BigInteger n = new BigInteger(num);
 				expectations.add(n);
 			}
-            reader.close();
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла: " + e.getMessage());
-        }
-        return expectations;
-    }
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("Ошибка при чтении файла: " + e.getMessage());
+		}
+		return expectations;
+	}
 
 	public String expectations_path = "src/test/resources/tests/expectations.txt";
 
-    List<BigInteger> expectations = readExpectations(expectations_path);
+	List<BigInteger> expectations = readExpectations(expectations_path);
 
-    @Test
-    public void testMin() throws IOException {
+	@Test
+	public void testMin() throws IOException {
 		String filePath = "src/test/resources/tests/test_min.txt";
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
 		String[] nums = reader.readLine().split(" ");
-        Assertions.assertEquals(expectations.get(0), App._min(nums));
-    }
+		Assertions.assertEquals(expectations.get(0), App._min(nums));
+	}
 
 	@Test
-    public void testMax() throws IOException {
+	public void testMax() throws IOException {
 		String filePath = "src/test/resources/tests/test_max.txt";
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
 		String[] nums = reader.readLine().split(" ");
-        Assertions.assertEquals(expectations.get(1), App._max(nums));
-    }
+		Assertions.assertEquals(expectations.get(1), App._max(nums));
+	}
 
 	@Test
-    public void testSum() throws IOException {
+	public void testSum() throws IOException {
 		String filePath = "src/test/resources/tests/test_sum.txt";
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
 		String[] nums = reader.readLine().split(" ");
-        Assertions.assertEquals(expectations.get(2), App._sum(nums));
-    }
+		Assertions.assertEquals(expectations.get(2), App._sum(nums));
+	}
 
 	@Test
-    public void testMult() throws IOException {
+	public void testMult() throws IOException {
 		String filePath = "src/test/resources/tests/test_mult.txt";
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
 		String[] nums = reader.readLine().split(" ");
-        Assertions.assertEquals(expectations.get(3), App._mult(nums));
-    }
+		Assertions.assertEquals(expectations.get(3), App._mult(nums));
+	}
 
 	@Test
-	public void testEmpty() throws IOException {
-		String filePath = "src/test/resources/tests/test_empty.txt";
-		try {
-			FileWriter writer = new FileWriter(filePath);
-			writer.close();
-			App.main(new String[]{filePath});
-		} catch (NullPointerException e) {
-			System.out.println("Получен пустой файл. Данные о ошибке: " + e.getMessage());;
-		}
+	public void testEmptyFile() {
+		String filename = "src/test/resources/tests/test_empty.txt";
+		String expectarion = "Файл пуст или содержит только пробельные символы. Пожалуйста, "
+			+ "проверьте файл 'input.txt'";
+		// Перенаправляем стандартный вывод в ByteArrayOutputStream
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		PrintStream originalOut = System.out;
+		System.setOut(new PrintStream(outContent));
+		// Запускаем main
+		App.main(new String[]{filename});
+		// Восстанавливаем стандартный вывод
+		System.setOut(originalOut);
+		Assertions.assertEquals(
+			expectarion,
+			outContent.toString().trim());
 	}
 }
